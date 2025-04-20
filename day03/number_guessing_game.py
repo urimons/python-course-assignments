b9f6ddf2ba799013f62e0a37377213b5f69241e6
Uri Monsonego <86400083+urimons@users.noreply.github.com>
2025-04-20 09:30:55 +0300

    Update and rename number_guessing_game to number_guessing_game.py

--- day03/number_guessing_game.py
+++ day03/number_guessing_game.py
@@ -3,8 +3,8 @@ import random
 random_number = random.randint(1, 20)
 guess = int(input("Please guess a number between 1 to 20:"))
 if guess < random_number:
-    print ("Your choosen number is too low!")
+    print ("Your chosen number is too low!")
 elif guess > random_number:
-    print("Your choosen number is too high!")
+    print("Your chosen number is too high!")
 else:
     print("Correct answer! What are the chances?!")
